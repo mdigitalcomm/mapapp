b7d9b3458ea00822b51aee2a0c25071b30ce0085
mdigitalcomm <mdigitalcomm@gmail.com>
2018-05-16 12:58:31 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+
+  render() {
+    let map;
+
+    return (
+      <div id = "map" className="App">
+        <h1>Map Goes here</h1>        
+      </div>
+    );
+  }
+}
+
+export default App;
