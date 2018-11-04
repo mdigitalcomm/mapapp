684f4a22d32213ef24e2930683e881eb5c5e49bb
MEOW <mdigitalcomm@gmail.com>
2018-11-04 14:42:08 -0500

    Revert "separated map from list view"
    This reverts commit e26328d4667dd81317ae19392748c0ba6bbe8b26.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import List from './List'
+import Map from './Map'
 
 class App extends Component {
 
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <List />     
+        <Map />     
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -1,28 +1,25 @@
+<<<<<<< HEAD
 import React, { Component } from 'react';
+||||||| e26328d... separated map from list view
+import { Component } from 'react';
+=======
+import React, { Component } from 'react'
+import { ButtonToolbar, SplitButton, MenuItem } from 'react-bootstrap'
+import InfoWindow from './InfoWindow'
+>>>>>>> parent of e26328d... separated map from list view
 
 class Map extends Component {
 	state = {
+		filter: '',
+		bookstores: [],
 		markers: []
 	}
-	
 
-	componentDidMount() {
+	componentDidMount() {		
 		/* Create the map */
 		let map = this.map()
 		/*Showing markers of all bookstores*/
-		let bookstores = this.props.listBookstores
-		this.addMarkers(bookstores, map)
-	}
-
-
-	componentWillReceiveProps(nextProps) {
-		const { listBookstores, refresh } = this.props
-		let map = this.map()
-		// console.log(refresh)
-		// console.log(nextProps.refresh)
-		if (nextProps.refresh == refresh) {
-			this.addMarkers(nextProps.listBookstores, map)
-		}
+		this.addMarkers(this.bookstores, map)
 	}
 
 	map = () => new window.google.maps.Map(document.getElementById('map'), {
@@ -32,10 +29,18 @@ class Map extends Component {
 			// Addd fitbounds method
 	})
 
-	addMarkers = (bookstores, map) => {
-		/*Create markers for bookstores*/
-		let markers = []
+	setFilter = (value) => {
+		let filterValue = new RegExp(value)
+		this.setState({filter: value})
+		setTimeout(() => {
+			let showingBookstores = this.bookstores.filter((bookstore) => filterValue.test(bookstore.state))
+			this.setState({bookstores: showingBookstores})
+		}, 100)
+	
+	}
 
+	addMarkers = (bookstores, map) => {
+		let markers = []
 		bookstores.map(bookstore => {			
 			let marker = new window.google.maps.Marker({
 				map: map,
@@ -114,12 +119,128 @@ class Map extends Component {
 				</div>
 				`
 		responseContainer.insertAdjacentHTML('afterbegin', htmlContent)
-	}	
-	
+	}
+
+	bookstores = [
+			{
+				title: "Politics and Prose Bookstore",
+				address: "5015 Connecticut Ave NW",
+				city: "Washington",
+				state: "D.C.",
+				lat : 38.955433406841465, 
+				lng : -77.06970870494843
+			},
+			{
+				title: "Washington Monument Bookstore",
+				address: "Washington Memorial Drwy SW",
+				city: "Washington",
+				state: "D.C.",
+				lat : 38.889276459346284, 
+				lng : -77.03189689261164
+			},
+			{
+				title: "George Mason University - Arlington Campus Bookstore",
+				address: "3401 Fairfax Dr",
+				city: "Arlington",
+				state: "VA",
+				lat : 38.88743859540963, 
+				lng : -77.09402561187744
+			},
+			{
+				title: "Georgetown University Bookstore",
+				address: "3800 Reservoir Rd NW",
+				city: "Washington",
+				state: "D.C.",
+				lat : 38.91015334940311, 
+				lng : -77.07438111305237
+			},
+			{
+				title: "NOVA Bookstore",
+				address: "8333 Little River Tpke",
+				city: "Annandale",
+				state: "VA",
+				lat : 38.83286355537448, 
+				lng : -77.23544332092266
+			},
+			{
+				title: "GW Bookstore",
+				address: "800 21st St NW",
+				city: "Washington",
+				state: "D.C.",
+				lat : 38.9001324513647, 
+				lng : -77.04667624668473
+			},
+			{
+				title: "Marymount University Bookstore",
+				address: "2807 N Glebe Rd",
+				city: "Arlington",
+				state: "VA",
+				lat : 38.90485705525619, 
+				lng : -77.1271488650243
+			},
+			{
+				title: "DeVry Crystal City Bookstore",
+				address: "2450 Crystal Dr",
+				city: "Arlington",
+				state: "VA",
+				lat : 38.85476415, 
+				lng : -77.05058921666667
+			}
+
+		]
+
 	render() {
+<<<<<<< HEAD
 		return (
 			<div id="map"></div>
 	)}
 }
 
 export default Map
+||||||| e26328d... separated map from list view
+		return null
+	}
+}
+=======
+		let listBookstores = this.state.filter ? this.state.bookstores : this.bookstores
+		return (
+			<div>
+				<div className="left">
+					<h1>Bookstores Near Me</h1>
+					<div className="search">							
+						<ButtonToolbar className="filter">
+							<SplitButton 
+								bsSize="large" 
+								title={this.state.filter? this.state.filter: "All Regions" } 
+								id="filter-button" 
+								onSelect={eventKey => {
+									this.setFilter(eventKey)
+									setTimeout(() => this.addMarkers(this.state.bookstores, this.map()), 200)
+								}}>
+								
+								<MenuItem eventKey="">All</MenuItem>
+								<MenuItem eventKey="VA">VA</MenuItem>
+								<MenuItem eventKey="D.C.">D.C.</MenuItem>
+							</SplitButton>
+						</ButtonToolbar>
+					</div>
+					<ul className="bookstores-list">
+						{listBookstores.map(bookstore => (
+								<li key={bookstore.title}>
+									<div onClick={(event) => this.matchMarker(event)} 
+										className="bookstore-name">{bookstore.title}</div>
+									<div className="bookstore-address">{bookstore.address}</div>
+								</li>
+							))
+						}
+						
+					</ul>				
+				</div>
+				<div id="map">
+				</div>				
+			</div>
+
+		)
+	}
+}
+>>>>>>> parent of e26328d... separated map from list view
