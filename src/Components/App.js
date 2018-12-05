07f38f9052925d56af88df1a6ffe92bdfd90c118
MEOW <mdigitalcomm@gmail.com>
2018-12-05 15:10:42 -0500

    Deleted redundant code

--- src/Components/App.js
+++ src/Components/App.js
@@ -226,11 +226,3 @@ class App extends Component {
 }
 
 export default App;
-
-// let loadJS = (src) => {
-// 		let ref = window.document.getElementsByTagName("script")[0]
-// 		let script = window.document.createElement("script")
-// 		script.src = src
-// 		script.async = true
-// 		ref.parentNode.insertBefore(script, ref)
-// 	}
